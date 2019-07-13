bdac9481f81bc887307a966e6c74afab8aa329cc
eric3u <eric3u@outlook.com>
2019-07-14 02:03:47 +0800

    Chore: 建立工程结构

---
+++ AWSLPlayer/AWSLPlayer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AWSLPlayer
+//
+//  Created by CmST0us on 2019/7/14.
+//  Copyright © 2019 eric3u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
---
+++ AWSLPlayer/AWSLPlayer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AWSLPlayer
+//
+//  Created by CmST0us on 2019/7/14.
+//  Copyright © 2019 eric3u. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
