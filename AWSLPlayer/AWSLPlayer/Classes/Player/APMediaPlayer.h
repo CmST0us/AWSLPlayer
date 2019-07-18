0eee32263fb243ca4eddf0e8d65cf2661b8d130a
eric3u <eric3u@outlook.com>
2019-07-19 00:29:29 +0800

    [Feat] WIP: 播放器控制交互设计<下班再晚也要回来更新代码> 根据自己心情，如果播放器控制层写的比价满意的话会把这层抽出去。 主要是要把flv的功能合进来，但是这就得再之前写的VideoCodecKit库里面做文章了。网络缓存方案，重连方案，等都得话时间去设计。 至于说弹幕等功能。。等自己排期吧，B站弹幕获取之前写了一个跨平台的，和iOS的，到时候接进来就行了。其他平台还在考虑中。

---
+++ AWSLPlayer/AWSLPlayer/Classes/Player/APMediaPlayer.h
@@ -0,0 +1,17 @@
+//
+//  APMediaPlayer.h
+//  AWSLPlayer
+//
+//  Created by CmST0us on 2019/7/19.
+//  Copyright © 2019 eric3u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APMediaPlayer : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AWSLPlayer/AWSLPlayer/Classes/Player/DisplayLayer/APMediaPlayerDisplayLayer.m
@@ -0,0 +1,13 @@
+//
+//  APMediaPlayerDisplayLayer.m
+//  AWSLPlayer
+//
+//  Created by CmST0us on 2019/7/19.
+//  Copyright © 2019 eric3u. All rights reserved.
+//
+
+#import "APMediaPlayerDisplayLayer.h"
+
+@implementation APMediaPlayerDisplayLayer
+
+@end
