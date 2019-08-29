78a421f316ee7a61ba9d5aee2cba73c09dbe4d9a
eric3u <eric3u@outlook.com>
2019-08-30 00:33:27 +0800

    [Feat] 添加图标

--- AWSLPlayer/AWSLPlayer/Classes/AppDelegate.m
+++ AWSLPlayer/AWSLPlayer/Classes/AppDelegate.m
@@ -20,7 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window = [[UIWindow alloc] init];
     
     self.hp = [[APHomepageViewController alloc] initWithStyle:UITableViewStyleGrouped];
     APNavigationController *nav = [[APNavigationController alloc] initWithRootViewController:self.hp];
